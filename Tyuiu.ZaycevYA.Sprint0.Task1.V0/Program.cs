5fef905a86c99d002ef235dfbffaeae00762975a
Redeks Gg <ddredeks@gmail.com>
2024-09-12 22:11:53 +0500

    Task1

---
+++ Tyuiu.ZaycevYA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ZaycevYA.Sprint0.Task1.V0
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            Console.WriteLine("Hello,World");
+            string name = "Ярослав";
+            int age = 18;
+            Console.ReadKey();
+            Console.WriteLine("Меня зовут " + name +   " мне " + age  + " лет !!!");
+            Console.ReadKey();
+
+
+        }
+
+    }
+
+
+
+
+
+
+
+}
+
